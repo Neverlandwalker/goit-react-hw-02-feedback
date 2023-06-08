8e2e7e2c383f98966639d773e4ba3b02bc8f1b5c
Neverlandwalker <Blonnyya@gmail.com>
2023-06-08 10:04:46 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,67 @@
+import { Component } from 'react';
+import { FeedbackForm } from 'components/FeedbackForm/FeedbackForm';
+import { Statistics } from 'components/Statistics/Statistics';
+import { Notification } from 'components/Notification/Notification';
+import { Container } from './App.styled';
+
+
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+
+
+  onLeaveFeedback = (event) => {
+    if (event === 'good') {
+      this.setState({ good: this.state.good + 1 })
+    }
+    else if (event === 'bad') {
+      this.setState({ bad: this.state.bad + 1 })
+    }
+    else if (event === 'neutral') {
+      this.setState({ neutral: this.state.neutral + 1 })
+    }
+  }
+  
+  countTotalFeedback = () => {
+    const totalFeedback = this.state.good + this.state.bad + this.state.neutral;
+    return totalFeedback; 
+  }
+
+ 
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  }
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <Container>
+          
+        <FeedbackForm
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={this.onLeaveFeedback}
+        />
+      
+        {this.countTotalFeedback() > 0 ? (
+        
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              totalFeedback={this.countTotalFeedback()}
+              positiveFeedbackPercentage={this.countPositiveFeedbackPercentage()}
+            />
+        
+        ) : (
+          <Notification message="There is no feedback." />
+        )}
+ 
+      </Container>
+    );
+  }
+}
---
+++ src/components/FeedbackForm/FeedbackForm.jsx
@@ -0,0 +1,35 @@
+import { PropTypes } from 'prop-types';
+import { FeedbackButtonList, FeedbackButton } from 'components/FeedbackForm/FeedbackForm.styled';
+import { Section } from 'components/Section/Section';
+
+
+
+
+export const FeedbackForm = ({ options, onLeaveFeedback }) => {
+    return (
+        <Section title='Please, leave Feedback'>
+            <FeedbackButtonList>
+                {options.map(( option, index ) =>  (
+                        <li key={index}>
+                            <FeedbackButton type='button' onClick={() => onLeaveFeedback(option)}>
+                                {option}
+                        </FeedbackButton>
+                        </li>
+                    
+                )
+                )}
+                
+            </FeedbackButtonList>
+        </Section>
+    );
+};
+
+
+
+
+
+
+FeedbackForm.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
